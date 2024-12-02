3d0929081d8b8314e600cd0fcde227ab3c47e863
awithliee <alijr282006@gmail.com>
2024-12-02 21:13:26 +0700

    mengerjakan tugas 6

---
+++ Latihan/Latihan 6/script.js
@@ -0,0 +1,7 @@
+const toogle = document.querySelector('.toggle input');
+const nav = document.querySelector('nav ul');
+
+
+toogle.addEventListener('click',function(){
+nav.classList.toggle('slide');
+});
